b04b2ac4c43b5f950508283a59748e56b0294d96
Joshua Rose <joshuajrose98@gmail.com>
2019-07-21 19:15:25 -0400

    fuck a beat i was tryna beat a case

--- js/agency.js
+++ js/agency.js
@@ -35,8 +35,3 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
-
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-                event.preventDefault();
-                $(this).ekkoLightbox();
-            });
